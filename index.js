e61b9f1dd78a0d01af8a3adc4d53d7f864a23dd2
Evan Lucas <evanlucas@me.com>
2014-06-20 17:26:21 -0500

    Remove old cover script
    switch to blanket

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-module.exports = process.env.GEPO_COV
-  ? require('./lib-cov')
-  : require('./lib')
+module.exports = require('./lib')
--- scripts/cover.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf lib-cov
-jscoverage lib lib-cov
-GEPO_COV=1 mocha -R html-cov test.js > coverage.html
-GEPO_COV=1 mocha -R json-cov test.js > coverage.json
-jostle coverage.json
